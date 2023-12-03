27c914cc82f520c2591859c5fc87bbd3738ea0f5
josephmassgithub45 <JOSEPHMASSAQUOI45@GMAIL.COM>
2023-12-03 12:05:59 +0000

    first commit

--- TEXT_APP.py
+++ TEXT_APP.py
@@ -9,9 +9,22 @@ class TEXT_APP:
         mainwindow=Tk()
         mainwindow.title(self.apptitle)
         mainwindow.configure(background=self.appcolor)
+        mainwindow.geometry('800x550')
+        mainwindow.resizable(False,False)
 
+        def read():
+            textfile=open("DATABASE.txt")
+            notes=textfile.read()
+            textarea.insert(END,notes)
 
+       
 
+        textarea=Text(mainwindow,width=50)
+        textarea.grid(row=0,column=2)
+        
+
+        openbutton=Button(mainwindow,text="OPEN",action=read())
+        openbutton.grid(row=1,column=2)
 
 
         mainwindow.mainloop()
